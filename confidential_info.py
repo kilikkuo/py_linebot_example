8478cca428987675c29e6be57bf74071ae262770
Kilik Kuo <kilik.kuo@gmail.com>
2018-02-27 17:12:07 +0800

    Add confidential_info.py to provide your own information

---
+++ confidential_info.py
@@ -0,0 +1,48 @@
+
+def convert_weekday_to_str(weekday):
+    assert 0 <= weekday <= 6
+    weekdaystr = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+    return weekdaystr[weekday]
+
+class ZumbaAccountInfo(object):
+    def __init__(self):
+        self.__username = ''
+        self.__pwd = ''
+        self.__login_url = ''
+        self.__schedules_url = ''
+        pass
+
+    def login_url(self):
+        return self.__login_url
+    
+    def schedules_url_payload(self, start, end):
+        payload = {}
+        return self.__schedules_url, payload
+
+    def username(self):
+        return self.__username
+    
+    def password(self):
+        return self.__pwd
+    
+    def process_customized_info(self, sp):
+        return None
+
+zumbaInfo = ZumbaAccountInfo()
+
+class LineBotInfo(object):
+    def __init__(self):
+        self.__token = 
+        self.__test_user_ids = []
+        self.__target_user_ids = []
+
+    def token(self):
+        return self.__token
+
+    def test_user_ids(self):
+        return self.__test_user_ids
+    
+    def target_user_ids(self):
+        return self.__target_user_ids
+
+botInfo = LineBotInfo()
